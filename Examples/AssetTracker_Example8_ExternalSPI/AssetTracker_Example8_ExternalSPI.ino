e03bee9247e0790e9828f1e09c5375062282df68
PaulZC <pc235603@gmail.com>
2020-11-03 15:45:01 +0000

    Added Example8 External SPI (Micro OLED)

---
+++ Examples/AssetTracker_Example8_ExternalSPI/AssetTracker_Example8_ExternalSPI.ino
@@ -0,0 +1,91 @@
+/*
+
+  MicroMod Asset Tracker Example
+  ==============================
+
+  External SPI
+
+  Written by: Paul Clark
+  Date: October 30th 2020
+
+  This example tests the external SPI pins using a Micro OLED display:
+  https://www.sparkfun.com/products/13003
+  Connect:
+  Display  Asset_Tracker
+  GND      GND
+  VCC      3.3V
+  DO       SCK
+  DI       COPI
+  DC       A0
+  CS       D0
+  RES      D1
+
+  The pins and ports are defined in AssetTrackerPins.ino.
+
+  Please make sure that you have selected the correct Board using the Tools\Board menu:
+  
+  Please add these lines to your File\Preferences\Additional Boards Manager URLs:
+  https://raw.githubusercontent.com/sparkfun/Arduino_Boards/master/IDE_Board_Manager/package_sparkfun_index.json
+  https://raw.githubusercontent.com/espressif/arduino-esp32/gh-pages/package_esp32_index.json
+  Then:
+  SparkFun Artemis MicroMod: Click here to get the boards: http://boardsmanager/All#SparkFun_Apollo3
+  SparkFun SAMD51 MicroMod : Click here to get the boards: http://boardsmanager/All#Arduino_SAMD_Boards plus http://boardsmanager/All#SparkFun_SAMD_Boards
+  SparkFun ESP32 MicroMod  : Click here to get the boards: http://boardsmanager/All#ESP32 (Please install the Espressif ESP32 boards _and_ the SparkFun ESP32 boards)
+
+  Special note for the ESP32:
+    If you are using the ESP32 Processor Board, you must open the G3/IMU_PWR and G4/RI split pads on the rear of the PCB
+    otherwise the PB will not be able to communicate with the SARA via serial.
+
+  Feel like supporting open source hardware?
+  Buy a board from SparkFun!
+  SparkFun MicroMod Artemis Processor : http://www.sparkfun.com/products/16401
+  SparkFun MicroMod SAMD51 Processor  : http://www.sparkfun.com/products/16791
+  SparkFun MicroMod ESP32 Processor   : http://www.sparkfun.com/products/16781
+  SparkFun MicroMod ATP Carrier Board          : https://www.sparkfun.com/products/16885
+  SparkFun MicroMod Data Logging Carrier Board : https://www.sparkfun.com/products/16829
+
+  Licence: MIT
+  Please see LICENSE.md for full details
+
+*/
+
+#include "AssetTrackerPins.h" // Include the Asset Tracker pin and port definitions
+
+#define SERIAL_PORT Serial // This is the console serial port - change this if required
+
+#include <SPI.h>
+
+#include <String.h>
+#include <SFE_MicroOLED.h>  // Click here to get the library: http://librarymanager/All#SparkFun_Micro_OLED
+
+MicroOLED oled(D1, A0, D0); // PIN_RESET, PIN_DC, PIN_CS
+
+void setup()
+{
+  initializeAssetTrackerPins(); // Initialize the pins and ports (defined in AssetTrackerPins.ino)
+
+  SERIAL_PORT.begin(115200); // Start the serial console
+  SERIAL_PORT.println(F("Asset Tracker Example"));
+
+  delay(100);
+
+  while (SERIAL_PORT.available()) // Make sure the serial RX buffer is empty
+    SERIAL_PORT.read();
+
+  SERIAL_PORT.println(F("Press any key to continue..."));
+
+  while (!SERIAL_PORT.available()) // Wait for the user to press a key (send any serial character)
+    ;
+
+  digitalWrite(LED_BUILTIN, HIGH);
+
+  // Display the splash screen
+  oled.begin();
+  oled.clear(ALL);
+  oled.display();
+}
+
+void loop()
+{
+  // Nothing to do here...
+}
