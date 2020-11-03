f0d9f7bb262eca7a84139399f85c732cf2b18711
PaulZC <pc235603@gmail.com>
2020-11-03 14:22:46 +0000

    Added Example6 IO Pins

---
+++ Examples/AssetTracker_Example6_IOPins/AssetTracker_Example6_IOPins.ino
@@ -0,0 +1,89 @@
+/*
+
+  MicroMod Asset Tracker Example
+  ==============================
+
+  IO Pins
+
+  Written by: Paul Clark
+  Date: October 30th 2020
+
+  This example tests the D0/D1/A0/A1/PWM0/PWM1 IO pins.
+
+  The pins and ports are defined in AssetTrackerPins.ino.
+
+  Please make sure that you have selected the correct Board using the Tools\Board menu:
+  
+  Please add these lines to your File\Preferences\Additional Boards Manager URLs:
+  https://raw.githubusercontent.com/sparkfun/Arduino_Boards/master/IDE_Board_Manager/package_sparkfun_index.json
+  https://raw.githubusercontent.com/espressif/arduino-esp32/gh-pages/package_esp32_index.json
+  Then:
+  SparkFun Artemis MicroMod: Click here to get the boards: http://boardsmanager/All#SparkFun_Apollo3
+  SparkFun SAMD51 MicroMod : Click here to get the boards: http://boardsmanager/All#Arduino_SAMD_Boards plus http://boardsmanager/All#SparkFun_SAMD_Boards
+  SparkFun ESP32 MicroMod  : Click here to get the boards: http://boardsmanager/All#ESP32 (Please install the Espressif ESP32 boards _and_ the SparkFun ESP32 boards)
+
+  Special note for the ESP32:
+    If you are using the ESP32 Processor Board, you must open the G3/IMU_PWR and G4/RI split pads on the rear of the PCB
+    otherwise the PB will not be able to communicate with the SARA via serial.
+
+  Feel like supporting open source hardware?
+  Buy a board from SparkFun!
+  SparkFun MicroMod Artemis Processor : http://www.sparkfun.com/products/16401
+  SparkFun MicroMod SAMD51 Processor  : http://www.sparkfun.com/products/16791
+  SparkFun MicroMod ESP32 Processor   : http://www.sparkfun.com/products/16781
+  SparkFun MicroMod ATP Carrier Board          : https://www.sparkfun.com/products/16885
+  SparkFun MicroMod Data Logging Carrier Board : https://www.sparkfun.com/products/16829
+
+  Licence: MIT
+  Please see LICENSE.md for full details
+
+*/
+
+#include "AssetTrackerPins.h" // Include the Asset Tracker pin and port definitions
+
+#define SERIAL_PORT Serial // This is the console serial port - change this if required
+
+void setup()
+{
+  initializeAssetTrackerPins(); // Initialize the pins and ports (defined in AssetTrackerPins.ino)
+
+  SERIAL_PORT.begin(115200); // Start the serial console
+  SERIAL_PORT.println(F("Asset Tracker Example"));
+
+  delay(100);
+
+  while (SERIAL_PORT.available()) // Make sure the serial RX buffer is empty
+    SERIAL_PORT.read();
+
+  SERIAL_PORT.println(F("Press any key to continue..."));
+
+  while (!SERIAL_PORT.available()) // Wait for the user to press a key (send any serial character)
+    ;
+
+  digitalWrite(LED_BUILTIN, HIGH);
+
+  // Set all six pins to OUTPUT
+  pinMode(D0, OUTPUT);
+  pinMode(D1, OUTPUT);
+  pinMode(A0, OUTPUT);
+  pinMode(A1, OUTPUT);
+  pinMode(PWM0, OUTPUT);
+  pinMode(PWM1, OUTPUT);
+}
+
+void loop()
+{
+  // Invert each pin in turn
+  digitalWrite(D0, !digitalRead(D0));
+  delay(1);
+  digitalWrite(D1, !digitalRead(D1));
+  delay(1);
+  digitalWrite(A0, !digitalRead(A0));
+  delay(1);
+  digitalWrite(A1, !digitalRead(A1));
+  delay(1);
+  digitalWrite(PWM0, !digitalRead(PWM0));
+  delay(1);
+  digitalWrite(PWM1, !digitalRead(PWM1));
+  delay(1);
+}
