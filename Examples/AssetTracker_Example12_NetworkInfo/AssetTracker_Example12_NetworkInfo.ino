c4a66887cbf19eef8e8f1572f4c0fa5c8436876d
PaulZC <pc235603@gmail.com>
2020-11-05 16:26:33 +0000

    Adding Example12 Network Info and Example13 Identification

---
+++ Examples/AssetTracker_Example12_NetworkInfo/AssetTracker_Example12_NetworkInfo.ino
@@ -0,0 +1,158 @@
+/*
+
+  MicroMod Asset Tracker Example
+  ==============================
+
+  Network Info
+
+  Written by: Paul Clark
+  Date: October 30th 2020
+
+  This example demonstrates how to register the SARA on the selected network.
+
+  The pins and ports are defined in AssetTrackerPins.ino.
+
+  Please make sure that you have selected the correct Board using the Tools\Board menu:
+  
+  Please add these lines to your File\Preferences\Additional Boards Manager URLs:
+  https://raw.githubusercontent.com/sparkfun/Arduino_Boards/master/IDE_Board_Manager/package_sparkfun_index.json
+  https://raw.githubusercontent.com/espressif/arduino-esp32/gh-pages/package_esp32_index.json
+  Then:
+  SparkFun Artemis MicroMod: Click here to get the boards: http://boardsmanager/All#SparkFun_Apollo3
+  SparkFun SAMD51 MicroMod : Click here to get the boards: http://boardsmanager/All#Arduino_SAMD_Boards plus http://boardsmanager/All#SparkFun_SAMD_Boards
+  SparkFun ESP32 MicroMod  : Click here to get the boards: http://boardsmanager/All#ESP32 (Please install the Espressif ESP32 boards _and_ the SparkFun ESP32 boards)
+
+  Special note for the ESP32:
+    If you are using the ESP32 Processor Board, you must open the G3/IMU_PWR and G4/RI split pads on the rear of the PCB
+    otherwise the PB will not be able to communicate with the SARA via serial.
+
+  Feel like supporting open source hardware?
+  Buy a board from SparkFun!
+  SparkFun MicroMod Artemis Processor : http://www.sparkfun.com/products/16401
+  SparkFun MicroMod SAMD51 Processor  : http://www.sparkfun.com/products/16791
+  SparkFun MicroMod ESP32 Processor   : http://www.sparkfun.com/products/16781
+  SparkFun MicroMod ATP Carrier Board          : https://www.sparkfun.com/products/16885
+  SparkFun MicroMod Data Logging Carrier Board : https://www.sparkfun.com/products/16829
+
+  Licence: MIT
+  Please see LICENSE.md for full details
+
+*/
+
+#include "AssetTrackerPins.h" // Include the Asset Tracker pin and port definitions
+
+#define SERIAL_PORT Serial // This is the console serial port - change this if required
+
+#include <SparkFun_u-blox_SARA-R5_Arduino_Library.h> //Click here to get the library: http://librarymanager/All#SparkFun_u-blox_SARA-R5_Arduino_Library
+
+// Create a SARA_R5 object to use throughout the sketch. Pass it the power pin number.
+SARA_R5 assetTracker(SARA_PWR);
+
+// Map registration status messages to more readable strings
+String registrationString[] =
+{
+  "Not registered",                         // 0
+  "Registered, home",                       // 1
+  "Searching for operator",                 // 2
+  "Registration denied",                    // 3
+  "Registration unknown",                   // 4
+  "Registered, roaming",                    // 5
+  "Registered, home (SMS only)",            // 6
+  "Registered, roaming (SMS only)",         // 7
+  "Registered, emergency service only",     // 8
+  "Registered, home, CSFB not preferred",   // 9
+  "Registered, roaming, CSFB not prefered"  // 10
+};
+
+// Network operator can be set to e.g.:
+// MNO_SW_DEFAULT -- DEFAULT
+// MNO_SIM_ICCID -- SIM DEFAULT
+// MNO_ATT -- AT&T 
+// MNO_VERIZON -- Verizon
+// MNO_TELSTRA -- Telstra
+// MNO_TMO -- T-Mobile US
+// MNO_CT -- China Telecom
+// MNO_VODAFONE
+// MNO_STD_EUROPE
+const mobile_network_operator_t MOBILE_NETWORK_OPERATOR = MNO_SIM_ICCID;
+
+// APN -- Access Point Name. Gateway between GPRS MNO
+// and another computer network. E.g. "hologram
+//const String APN = "hologram";
+
+// The APN can be omitted: this is the so-called "blank APN" setting that may be suggested by
+// network operators (e.g. to roaming devices); in this case the APN string is not included in
+// the message sent to the network.
+const String APN = "";
+
+void setup()
+{
+  initializeAssetTrackerPins(); // Initialize the pins and ports (defined in AssetTrackerPins.ino)
+
+  SERIAL_PORT.begin(115200); // Start the serial console
+  SERIAL_PORT.println(F("Asset Tracker Example"));
+  SERIAL_PORT.println();
+  SERIAL_PORT.println(F("Initializing the SARA-R5..."));
+  SERIAL_PORT.println(F("...this may take ~25 seconds if the SARA is off."));
+  SERIAL_PORT.println(F("...it may take ~5 seconds if it just turned on."));
+  SERIAL_PORT.println();
+
+  delay(100);
+
+  while (SERIAL_PORT.available()) // Make sure the serial RX buffer is empty
+    SERIAL_PORT.read();
+
+  SERIAL_PORT.println(F("Press any key to continue..."));
+
+  while (!SERIAL_PORT.available()) // Wait for the user to press a key (send any serial character)
+    ;
+
+  assetTracker.enableDebugging(SERIAL_PORT); // Uncomment this line to enable helpful debug messages
+
+  assetTracker.invertPowerPin(true); // For the Asset Tracker, we need to invert the power pin so it pulls high instead of low
+
+  // Initialize the SARA using SARA_Serial and 9600 Baud
+  // begin calls init. init sets: SARA GPIO1 to NETWORK_STATUS, and SARA GPIO6 to TIME_PULSE_OUTPUT
+  if (assetTracker.begin(SARA_Serial, 9600) )
+  {
+    SERIAL_PORT.println(F("Asset Tracker (SARA-R5) connected!"));
+  }
+  else
+  {
+    SERIAL_PORT.println(F("Unable to communicate with the SARA."));
+    SERIAL_PORT.println(F("Manually power-on (hold the SARA On button for 3 seconds) on and try again."));
+    while (1) ; // Loop forever on fail
+  }
+  SERIAL_PORT.println();
+
+  if (!assetTracker.setNetwork(MOBILE_NETWORK_OPERATOR))
+  {
+    SERIAL_PORT.println(F("Error setting network. Try cycling the power."));
+    while (1) ;
+  }
+  
+  if (assetTracker.setAPN(APN) == SARA_R5_SUCCESS)
+  {
+    SERIAL_PORT.println(F("APN successfully set."));
+  }
+  
+  SERIAL_PORT.println(F("Network set. Ready to go!"));
+  
+  // RSSI: Received signal strength:
+  SERIAL_PORT.println("RSSI: " + String(assetTracker.rssi()));
+  // Registration Status
+  int regStatus = assetTracker.registration();
+  if ((regStatus >= 0) && (regStatus <= 10))
+  {
+    SERIAL_PORT.println("Network registration: " + registrationString[regStatus]);
+  }
+  
+  if (regStatus > 0) {
+    SERIAL_PORT.println(F("All set. Go to the next example!"));
+  }
+}
+
+void loop()
+{
+  // Do nothing. Now that we're registered move on to the next example.
+}
---
+++ Examples/AssetTracker_Example13_Identification/AssetTracker_Example13_Identification.ino
@@ -0,0 +1,114 @@
+/*
+
+  MicroMod Asset Tracker Example
+  ==============================
+
+  Identification
+
+  Written by: Paul Clark
+  Date: October 30th 2020
+
+  This example demonstrates how to read the SARA's:
+    Manufacturer identification
+    Model identification
+    Firmware version identification
+    Product Serial No.
+    IMEI identification
+    IMSI identification
+    SIM CCID
+    Subscriber number
+    Capabilities
+
+  The pins and ports are defined in AssetTrackerPins.ino.
+
+  Please make sure that you have selected the correct Board using the Tools\Board menu:
+  
+  Please add these lines to your File\Preferences\Additional Boards Manager URLs:
+  https://raw.githubusercontent.com/sparkfun/Arduino_Boards/master/IDE_Board_Manager/package_sparkfun_index.json
+  https://raw.githubusercontent.com/espressif/arduino-esp32/gh-pages/package_esp32_index.json
+  Then:
+  SparkFun Artemis MicroMod: Click here to get the boards: http://boardsmanager/All#SparkFun_Apollo3
+  SparkFun SAMD51 MicroMod : Click here to get the boards: http://boardsmanager/All#Arduino_SAMD_Boards plus http://boardsmanager/All#SparkFun_SAMD_Boards
+  SparkFun ESP32 MicroMod  : Click here to get the boards: http://boardsmanager/All#ESP32 (Please install the Espressif ESP32 boards _and_ the SparkFun ESP32 boards)
+
+  Special note for the ESP32:
+    If you are using the ESP32 Processor Board, you must open the G3/IMU_PWR and G4/RI split pads on the rear of the PCB
+    otherwise the PB will not be able to communicate with the SARA via serial.
+
+  Feel like supporting open source hardware?
+  Buy a board from SparkFun!
+  SparkFun MicroMod Artemis Processor : http://www.sparkfun.com/products/16401
+  SparkFun MicroMod SAMD51 Processor  : http://www.sparkfun.com/products/16791
+  SparkFun MicroMod ESP32 Processor   : http://www.sparkfun.com/products/16781
+  SparkFun MicroMod ATP Carrier Board          : https://www.sparkfun.com/products/16885
+  SparkFun MicroMod Data Logging Carrier Board : https://www.sparkfun.com/products/16829
+
+  Licence: MIT
+  Please see LICENSE.md for full details
+
+*/
+
+#include "AssetTrackerPins.h" // Include the Asset Tracker pin and port definitions
+
+#define SERIAL_PORT Serial // This is the console serial port - change this if required
+
+#include <SparkFun_u-blox_SARA-R5_Arduino_Library.h> //Click here to get the library: http://librarymanager/All#SparkFun_u-blox_SARA-R5_Arduino_Library
+
+// Create a SARA_R5 object to use throughout the sketch. Pass it the power pin number.
+SARA_R5 assetTracker(SARA_PWR);
+
+void setup()
+{
+  initializeAssetTrackerPins(); // Initialize the pins and ports (defined in AssetTrackerPins.ino)
+
+  SERIAL_PORT.begin(115200); // Start the serial console
+  SERIAL_PORT.println(F("Asset Tracker Example"));
+  SERIAL_PORT.println();
+  SERIAL_PORT.println(F("Initializing the SARA-R5..."));
+  SERIAL_PORT.println(F("...this may take ~25 seconds if the SARA is off."));
+  SERIAL_PORT.println(F("...it may take ~5 seconds if it just turned on."));
+  SERIAL_PORT.println();
+
+  delay(100);
+
+  while (SERIAL_PORT.available()) // Make sure the serial RX buffer is empty
+    SERIAL_PORT.read();
+
+  SERIAL_PORT.println(F("Press any key to continue..."));
+
+  while (!SERIAL_PORT.available()) // Wait for the user to press a key (send any serial character)
+    ;
+
+  assetTracker.enableDebugging(SERIAL_PORT); // Uncomment this line to enable helpful debug messages
+
+  assetTracker.invertPowerPin(true); // For the Asset Tracker, we need to invert the power pin so it pulls high instead of low
+
+  // Initialize the SARA using SARA_Serial and 9600 Baud
+  // begin calls init. init sets: SARA GPIO1 to NETWORK_STATUS, and SARA GPIO6 to TIME_PULSE_OUTPUT
+  if (assetTracker.begin(SARA_Serial, 9600) )
+  {
+    SERIAL_PORT.println(F("Asset Tracker (SARA-R5) connected!"));
+  }
+  else
+  {
+    SERIAL_PORT.println(F("Unable to communicate with the SARA."));
+    SERIAL_PORT.println(F("Manually power-on (hold the SARA On button for 3 seconds) on and try again."));
+    while (1) ; // Loop forever on fail
+  }
+  SERIAL_PORT.println();
+
+  Serial.println("Manufacturer ID:     " + String(assetTracker.getManufacturerID()));
+  Serial.println("Model ID:            " + String(assetTracker.getModelID()));
+  Serial.println("Firmware Version:    " + String(assetTracker.getFirmwareVersion()));
+  Serial.println("Product Serial No.:  " + String(assetTracker.getSerialNo()));
+  Serial.println("IMEI:                " + String(assetTracker.getIMEI()));
+  Serial.println("IMSI:                " + String(assetTracker.getIMSI()));
+  Serial.println("SIM CCID:            " + String(assetTracker.getCCID()));
+  Serial.println("Subscriber No.:      " + String(assetTracker.getSubscriberNo()));
+  Serial.println("Capabilities:        " + String(assetTracker.getCapabilities()));
+}
+
+void loop()
+{
+  // Do nothing. Move on to the next example.
+}
